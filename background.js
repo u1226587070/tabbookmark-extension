1f818b255aee782267f556d84e60a6c9a16c2989
u1226587070 <u1226587070@gmail.com>
2026-06-07 05:38:58 +0000

    Add setup button to open shortcuts page directly

--- background.js
+++ background.js
@@ -15,9 +15,7 @@ async function getBookmarkBarItems() {
 function sendToActiveTab(action) {
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     if (tabs[0]?.id) {
-      chrome.tabs.sendMessage(tabs[0].id, { action }).catch(() => {
-        // Content script not loaded on this page (e.g. edge:// pages)
-      });
+      chrome.tabs.sendMessage(tabs[0].id, { action }).catch(() => {});
     }
   });
 }
@@ -32,20 +30,34 @@ chrome.commands.onCommand.addListener((command) => {
   }
 });
 
-// --- Messages from content script ---
+// --- All message handling in one listener ---
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  // Open bookmark
   if (msg.action === 'openBookmark') {
     getBookmarkBarItems().then(items => {
       const item = items[msg.index];
-      if (item?.url) {
-        chrome.tabs.create({ url: item.url });
-      }
+      if (item?.url) chrome.tabs.create({ url: item.url });
     });
     return false;
   }
 
+  // Get bookmarks
   if (msg.action === 'getBookmarks') {
     getBookmarkBarItems().then(items => sendResponse({ items }));
+    return true;
+  }
+
+  // Open shortcuts setup page (from setup.html button)
+  if (msg.action === 'openShortcuts') {
+    chrome.tabs.create({ url: 'edge://extensions/shortcuts' }).then(() => {
+      sendResponse({ ok: true });
+    }).catch(() => {
+      chrome.tabs.create({ url: 'chrome://extensions/shortcuts' }).then(() => {
+        sendResponse({ ok: true });
+      }).catch(() => {
+        sendResponse({ ok: false });
+      });
+    });
     return true;
   }
 });
